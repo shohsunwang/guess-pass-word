bab18c1647b08d6ffbf4878a0cfd651ea67948cd
Rick Wang <sho.hsun.wang@gmail.com>
2019-08-09 22:38:53 +0800

    add start/end num

--- guess.py
+++ guess.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1, 1000)
+start = input('請輸入請起始值: ')
+end = input('請輸入請結束值: ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
     count += 1 #count = count + 1
